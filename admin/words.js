6b7fc4fa15521bb4f585e5e0c03a3b0f906b1cd8
TA2k <tombox2020@gmail.com>
2021-11-03 10:41:10 +0100

    intial commit

---
+++ admin/words.js
@@ -0,0 +1,53 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "toyota adapter settings": {
+        "en": "Adapter settings for toyota",
+        "de": "Adaptereinstellungen für toyota",
+        "ru": "Настройки адаптера для toyota",
+        "pt": "Configurações do adaptador para toyota",
+        "nl": "Adapterinstellingen voor toyota",
+        "fr": "Paramètres d'adaptateur pour toyota",
+        "it": "Impostazioni dell'adattatore per toyota",
+        "es": "Ajustes del adaptador para toyota",
+        "pl": "Ustawienia adaptera dla toyota",
+        "zh-cn": "toyota的适配器设置"
+    },
+    "App Email": {
+        en: "App Email",
+        de: "App-E-Mail",
+        ru: "Электронная почта приложения",
+        pt: "Email do aplicativo",
+        nl: "App-e-mail",
+        fr: "Courriel de l'application",
+        it: "E-mail dell'app",
+        es: "Correo electrónico de la aplicación",
+        pl: "E-mail aplikacji",
+        "zh-cn": "应用电子邮件",
+    },
+    "App Password": {
+        en: "App Password",
+        de: "App-Passwort",
+        ru: "Пароль приложения",
+        pt: "Senha de app",
+        nl: "App-wachtwoord",
+        fr: "Mot de passe de l'application",
+        it: "Password dell'app",
+        es: "Contraseña de la aplicación",
+        pl: "Hasło do aplikacji",
+        "zh-cn": "应用密码",
+    },
+    "Update interval (in minutes)": {
+        en: "Update interval (in minutes)",
+        de: "Aktualisierungsintervall (in Minuten)",
+        ru: "Интервал обновления (в минутах)",
+        pt: "Intervalo de atualização (em minutos)",
+        nl: "Update-interval (in minuten)",
+        fr: "Intervalle de mise à jour (en minutes)",
+        it: "Intervallo di aggiornamento (in minuti)",
+        es: "Intervalo de actualización (en minutos)",
+        pl: "Interwał aktualizacji (w minutach)",
+        "zh-cn": "更新间隔（分钟）",
+    },
+};
---
+++ main.js
@@ -0,0 +1,313 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.0.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios");
+const Json2iob = require("./lib/json2iob");
+
+class Toyota extends utils.Adapter {
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    constructor(options) {
+        super({
+            ...options,
+            name: "toyota",
+        });
+        this.on("ready", this.onReady.bind(this));
+        this.on("stateChange", this.onStateChange.bind(this));
+        this.on("unload", this.onUnload.bind(this));
+        this.deviceArray = [];
+        this.json2iob = new Json2iob(this);
+        this.token = null;
+        this.uuid = null;
+        this.requestClient = axios.create();
+        this.updateInterval = null;
+        this.reLoginTimeout = null;
+        this.refreshTokenTimeout = null;
+    }
+
+    /**
+     * Is called when databases are connected and adapter received configuration.
+     */
+    async onReady() {
+        // Reset the connection indicator during startup
+        this.setState("info.connection", false, true);
+        if (this.config.interval < 0.5) {
+            this.log.info("Set interval to minimum 0.5");
+            this.config.interval = 0.5;
+        }
+        this.subscribeStates("*");
+
+        await this.login();
+
+        if (this.token && this.uuid) {
+            await this.getDeviceList();
+            await this.updateDevices();
+            this.updateInterval = setInterval(async () => {
+                await this.updateDevices();
+            }, this.config.interval * 60 * 1000);
+            this.refreshTokenInterval = setInterval(() => {
+                this.login();
+            }, 3500 * 1000);
+        }
+    }
+    async login() {
+        this.session_data = await this.requestClient({
+            method: "post",
+            url: "https://myt-agg.toyota-europe.com/cma/api/user/login",
+            headers: {
+                accept: "*/*",
+                "content-type": "application/json",
+                "x-tme-locale": "en-gb",
+                "x-tme-brand": "TOYOTA",
+                "x-tme-app-version": "4.5.0",
+                "user-agent": "MyT/4.5.0 iPhone10,5 iOS/14.8 CFNetwork/1240.0.4 Darwin/20.6.0",
+                "accept-language": "de-DE",
+            },
+            data: JSON.stringify({
+                password: this.config.password,
+                username: this.config.username,
+            }),
+        })
+            .then((res) => {
+                this.log.debug(JSON.stringify(res.data));
+                this.token = res.data.token;
+                if (res.data.customerProfile) {
+                    this.uuid = res.data.customerProfile.uuid;
+                } else {
+                    this.log.error("No uuid found");
+                }
+                this.setState("info.connection", true, true);
+            })
+            .catch((error) => {
+                this.log.error(error);
+                if (error.response) {
+                    this.log.error(JSON.stringify(error.response.data));
+                }
+            });
+    }
+    async getDeviceList() {
+        await this.requestClient({
+            method: "get",
+            url: "https://myt-agg.toyota-europe.com/cma/api/user/" + this.uuid + "/vehicle/details",
+            headers: {
+                cookie: "iPlanetDirectoryPro=" + this.token,
+                accept: "*/*",
+                "x-tme-locale": "de-de",
+                "x-tme-app-version": "4.5.0",
+                "user-agent": "MyT/4.5.0 iPhone10,5 iOS/14.8 CFNetwork/1240.0.4 Darwin/20.6.0",
+                "accept-language": "de-DE",
+                "x-tme-brand": "TOYOTA",
+            },
+        })
+            .then(async (res) => {
+                this.log.debug(JSON.stringify(res.data));
+
+                for (const device of res.data) {
+                    this.deviceArray.push(device.vin);
+                    const name = device.alias;
+                    await this.setObjectNotExistsAsync(device.vin, {
+                        type: "device",
+                        common: {
+                            name: name,
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(device.vin + ".remote", {
+                        type: "channel",
+                        common: {
+                            name: "Remote Controls",
+                        },
+                        native: {},
+                    });
+                    await this.setObjectNotExistsAsync(device.vin + ".general", {
+                        type: "channel",
+                        common: {
+                            name: "General Information",
+                        },
+                        native: {},
+                    });
+
+                    const remoteArray = [
+                        { command: "hvac", name: "True = Start, False = Stop" },
+                        { command: "hvac-temperature", name: "HVAC Temperature", type: "number", role: "value" },
+                    ];
+                    remoteArray.forEach((remote) => {
+                        this.setObjectNotExists(device.vin + ".remote." + remote.command, {
+                            type: "state",
+                            common: {
+                                name: remote.name || "",
+                                type: remote.type || "boolean",
+                                role: remote.role || "boolean",
+                                write: true,
+                                read: true,
+                            },
+                            native: {},
+                        });
+                    });
+                    this.json2iob.parse(device.vin + ".general", device);
+                }
+            })
+            .catch((error) => {
+                this.log.error(error);
+                error.response && this.log.error(JSON.stringify(error.response.data));
+            });
+    }
+
+    async updateDevices() {
+        const statusArray = [
+            {
+                path: "status",
+                url: "https://myt-agg.toyota-europe.com/cma/api/user/" + this.uuid + "/vehicle/$vin/vehicleStatus",
+                desc: "Status of the car",
+            },
+            {
+                path: "addtionalInfo",
+                url: "https://myt-agg.toyota-europe.com/cma/api/user/" + this.uuid + "/vehicle/$vin/addtionalInfo",
+                desc: "AddtionalInfo of the car",
+            },
+            {
+                path: "location",
+                url: "https://myt-agg.toyota-europe.com/cma/api/user/" + this.uuid + "/vehicle/$vin/location",
+                desc: "Location of the car",
+            },
+        ];
+
+        const headers = {
+            cookie: "iPlanetDirectoryPro=" + this.token,
+            accept: "*/*",
+            "x-tme-locale": "de-de",
+            "x-tme-app-version": "4.5.0",
+            "user-agent": "MyT/4.5.0 iPhone10,5 iOS/14.8 CFNetwork/1240.0.4 Darwin/20.6.0",
+            "accept-language": "de-DE",
+            "x-tme-brand": "TOYOTA",
+        };
+        this.deviceArray.forEach(async (vin) => {
+            statusArray.forEach(async (element) => {
+                const url = element.url.replace("$vin", vin);
+
+                await this.requestClient({
+                    method: "get",
+                    url: url,
+                    headers: headers,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        if (!res.data) {
+                            return;
+                        }
+                        const data = res.data;
+
+                        const forceIndex = null;
+                        const preferedArrayName = null;
+
+                        this.json2iob.parse(vin + "." + element.path, data, { forceIndex: forceIndex, preferedArrayName: preferedArrayName, channelName: element.desc });
+                    })
+                    .catch((error) => {
+                        if (error.response) {
+                            if (error.response.status === 401 || error.response.status === 403) {
+                                error.response && this.log.debug(JSON.stringify(error.response.data));
+                                this.log.info(element.path + " receive 401/403 error. Relogin in 5 minutes");
+                                this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+                                this.refreshTokenTimeout = setTimeout(() => {
+                                    this.login();
+                                }, 1000 * 60 * 5);
+
+                                return;
+                            }
+                        }
+                        this.log.error(url);
+                        this.log.error(error);
+                        error.response && this.log.error(JSON.stringify(error.response.data));
+                    });
+            });
+        });
+    }
+
+    /**
+     * Is called when adapter shuts down - callback has to be called under any circumstances!
+     * @param {() => void} callback
+     */
+    onUnload(callback) {
+        try {
+            this.setState("info.connection", false, true);
+            this.refreshTimeout && clearTimeout(this.refreshTimeout);
+            this.reLoginTimeout && clearTimeout(this.reLoginTimeout);
+            this.refreshTokenTimeout && clearTimeout(this.refreshTokenTimeout);
+            this.updateInterval && clearInterval(this.updateInterval);
+            this.refreshTokenInterval && clearInterval(this.refreshTokenInterval);
+            callback();
+        } catch (e) {
+            callback();
+        }
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State | null | undefined} state
+     */
+    async onStateChange(id, state) {
+        if (state) {
+            if (!state.ack) {
+                if (id.split(".")[3] !== "remote") {
+                    return;
+                }
+                const deviceId = id.split(".")[2];
+                const path = id.split(".")[4];
+                if (path === "hvac-temperature") {
+                    return;
+                }
+                const data = {};
+                const url = "https://myt-agg.toyota-europe.com/cma/api/user/" + this.uuid + "/vehicle/" + deviceId + "/remoteControl";
+                this.log.debug(JSON.stringify(data));
+                this.log.debug(url);
+                await this.requestClient({
+                    method: "post",
+                    url: url,
+                    headers: {
+                        cookie: "iPlanetDirectoryPro=" + this.token,
+                        accept: "*/*",
+                        "x-tme-locale": "de-de",
+                        "x-tme-app-version": "4.5.0",
+                        "user-agent": "MyT/4.5.0 iPhone10,5 iOS/14.8 CFNetwork/1240.0.4 Darwin/20.6.0",
+                        "accept-language": "de-DE",
+                        "x-tme-brand": "TOYOTA",
+                    },
+                    data: data,
+                })
+                    .then((res) => {
+                        this.log.debug(JSON.stringify(res.data));
+                        return res.data;
+                    })
+                    .catch((error) => {
+                        this.log.error(error);
+                        if (error.response) {
+                            this.log.error(JSON.stringify(error.response.data));
+                        }
+                    });
+                this.refreshTimeout && clearTimeout(this.refreshTimeout);
+                this.refreshTimeout = setTimeout(async () => {
+                    await this.updateDevices();
+                }, 10 * 1000);
+            }
+        }
+    }
+}
+
+if (require.main !== module) {
+    // Export the constructor in compact mode
+    /**
+     * @param {Partial<utils.AdapterOptions>} [options={}]
+     */
+    module.exports = (options) => new Toyota(options);
+} else {
+    // otherwise start the instance directly
+    new Toyota();
+}
